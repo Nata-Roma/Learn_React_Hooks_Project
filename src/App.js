19cf7409fee730c3c5c345a4ca2d12c9b3ac8820
Nata-Roma <67419233+Nata-Roma@users.noreply.github.com>
2020-11-06 14:28:43 +0200

    change class to function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./styles.css";
 
-class App extends Component {
-  state = {name: ''};
+const App = () => {
+  const [name, setName] = useState("");
 
-  changeHandler = (e) => {
-    this.setState({name: e.target.value});
-  }
+  const changeHandler = (e) => {
+    setName(e.target.value);
+  };
 
-  render() {
-    const {name} = this.state;
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>This is what you are typing: <span style={{color: 'red'}}>{name}</span></h2>
-        <input type="text" value={name} onChange={this.changeHandler}/>
-      </div>
-    );  
-  }
-  
-}
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>
+        This is what you are typing:{" "}
+        <span style={{ color: "red" }}>{name}</span>
+      </h2>
+      <input type="text" value={name} onChange={changeHandler} />
+    </div>
+  );
+};
 
 export default App;
+
+// Refactoring
+
+// class App extends Component {
+//   state = {name: ''};
+
+//   changeHandler = (e) => {
+//     this.setState({name: e.target.value});
+//   }
+
+//   render() {
+//     const {name} = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello CodeSandbox</h1>
+//         <h2>This is what you are typing: <span style={{color: 'red'}}>{name}</span></h2>
+//         <input type="text" value={name} onChange={this.changeHandler}/>
+//       </div>
+//     );
+//   }
+
+// }
