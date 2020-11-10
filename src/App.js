a315216f91edcdf713023f2162932e8921a5a320
Nata-Roma <67419233+Nata-Roma@users.noreply.github.com>
2020-11-10 16:13:00 +0200

    useRedusers combined in one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useState } from 'react';
 import CreatePost from './create-post';
 import Posts from './posts';
+import appReducer from './reducers';
 
 import './styles.css';
 import UserBlock from './user-block';
@@ -27,45 +28,24 @@ const actions = [
   }
 ];
 
-const userReducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-    case 'REGISTER':
-      return action.userName;
-    case 'LOGOUT':
-      return '';
-    default:
-      throw new Error();
-  }
-};
-
-const postsReducer = (state, action) => {
-  switch (action.type) {
-    case 'CREATE_POST':
-      const newPost = {
-        title: action.title,
-        content: action.content,
-        author: action.author
-      };
-      return [newPost, ...state];
-    default:
-      throw new Error();
-  }
-};
-
 const App = () => {
   // const [user, setUser] = useState('');
-  const [user, dispatchUser] = useReducer(userReducer, '');
+  // const [user, dispatchUser] = useReducer(userReducer, '');
   // const [posts, setPosts] = useState(defaultPosts);
-  const [posts, dispatchPosts] = useReducer(postsReducer, defaultPosts);
+  // const [posts, dispatchPosts] = useReducer(postsReducer, defaultPosts);
+
+  const [state, dispatch] = useReducer(appReducer, {
+    user: '',
+    posts: defaultPosts
+  });
+
+  const { user, posts } = state;
 
   return (
     <div className="App">
-      <UserBlock dispatch={dispatchUser} user={user} />
+      <UserBlock dispatch={dispatch} user={user} />
       <br />
-      {user && (
-        <CreatePost user={user} dispatch={dispatchPosts} posts={posts} />
-      )}
+      {user && <CreatePost user={user} dispatch={dispatch} posts={posts} />}
       <br />
       {user && <Posts posts={posts} />}
     </div>
